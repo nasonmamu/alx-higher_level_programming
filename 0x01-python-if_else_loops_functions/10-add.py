096a834564e6035cd1aafd5809511a15f47ae018
nasonmamu <nasonmamu94@gmail.com>
2023-11-28 09:28:06 -0800

    Contains the file of python

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 10-add.py
+# Nason Mamu
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Nason Mamu
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
