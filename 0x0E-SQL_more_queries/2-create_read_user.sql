f140fd1bf903ffe8cd2a9af5647faaad9e3508d9
nasonmamu <nasonmamu94@gmail.com>
2024-02-14 05:53:32 -0800

    updated the the code again

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,7 @@
--- Lists all shows and genres linked to the show from the
--- database hbtn_0d_tvshows.
--- Records are ordered by ascending show title and genre name.
-SELECT t.`title`, g.`name`
-  FROM `tv_shows` AS t
-       LEFT JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
-
-       LEFT JOIN `tv_genres` AS g
-       ON s.`genre_id` = g.`id`
- ORDER BY t.`title`, g.`name`;
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user user_0d_2 with password user_0d_2_pwd
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privilege in the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
