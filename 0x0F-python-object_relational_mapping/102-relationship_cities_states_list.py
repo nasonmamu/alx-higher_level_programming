e9749b0d9dbeb541b464260910bc14e116cf2857
nasonmamu <nasonmamu94@gmail.com>
2024-03-18 08:49:57 -0700

    Write a script that lists all City objects from the database hbtn_0e_101_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    Your script should connect to a MySQL server running on localhost at port 3306
    You must use only one query to the database
    You must use the state relationship to access to the State object linked to the City object
    Results must be sorted in ascending order by cities.id
    Results must be displayed as they are in the example below
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_101_usa.
-# Usage: ./102-relationship_cities_states_list.py <mysql username> /
-#                                                 <mysql password> /
-#                                                 <database name>
+"""
+Lists all City objects from the database hbtn_0e_101_usa.
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
+from relationship_state import Base, State
 from relationship_city import City
 
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create engine to interact with the database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, database))
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session
     session = Session()
 
-    for city in session.query(City).order_by(City.id):
+    # Query and print all City objects with their corresponding State name
+    cities = session.query(City).order_by(City.id).all()
+    for city in cities:
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
