6e2c1af3ae74e251bd5e20c0949ffc195bd285e7
nasonmamu <nasonmamu94@gmail.com>
2024-03-18 07:53:54 -0700

    Write a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa
    Your script should take 4 arguments: mysql username, mysql password, database name and state name to search (SQL injection free)
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    You can assume you have one record with the state name to search
    Results must display the states.id
    If no state has the name you searched for, display Not found
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
-# Lists the State object with the name passed as argument
-# from the database hbtn_0e_6_usa.
-# Usage: ./10-model_state_my_get.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
-#                                   <state name searched>
+"""
+Script that prints the State object with the name passed as an argument
+from the database hbtn_0e_6_usa.
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Create engine to interact with the database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, database))
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session
     session = Session()
 
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-    if found is False:
+    # Query for state with given name
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
         print("Not found")
