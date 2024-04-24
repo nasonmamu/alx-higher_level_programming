242d26c31e7952babafa633892896338fa7b808a
Nason mamu <nasonmamu94@gmail.com>
2024-04-24 17:05:10 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,21 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+
+const writeStringToFile = (filePath, content) => {
+  fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node script.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+writeStringToFile(filePath, content);
