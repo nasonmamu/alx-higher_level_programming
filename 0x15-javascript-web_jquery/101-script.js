3668c80b0d5521f6e1a5aa3369d3870b6260e989
Nason mamu <nasonmamu94@gmail.com>
2024-05-02 15:36:32 +0300

    Update 101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,24 @@
+$('document').ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
+
+.......................
+
+vi 102-script.js
+
+$('document').ready(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
+    });
+  });
+});
